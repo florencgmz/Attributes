24e7cd872658b60f83bdb5a9d79cd03fe16f86b4
Florencia Gomez <florencgmz@gmail.com>
2018-10-18 09:32:18 -0300

    Add files via upload
    status code

---
+++ ExampleAttributes/Attributes/DisplayNameAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExampleAttributes.Attributes
+{
+    
+    public class DisplayNameAttribute : Attribute
+    {
+        public readonly string DisplayName;
+
+        //constructor
+        public DisplayNameAttribute(string displayName)
+        {
+            DisplayName = displayName;
+        }
+    }
+}
---
+++ ExampleAttributes/Models/Fruit.cs
@@ -0,0 +1,20 @@
+﻿using ExampleAttributes.Attributes;
+
+namespace ExampleAttributes.Enums
+{
+    public enum Fruit
+    {
+        [DisplayNameAttribute("Lemon")]
+        Lemon,
+        [DisplayNameAttribute("Watermelon")]
+        Watermelon,
+        [DisplayNameAttribute("Orange")]
+        Orange,
+        [DisplayNameAttribute("BloodOrange")]
+        BloodOrange,
+        [DisplayNameAttribute("Kiwi")]
+        Kiwi,
+        [DisplayNameAttribute("Banana")]
+        Banana
+    }
+}
